a997f2c38c723c8ece0de80fb4ec853da9ba2993
Kasper Primdal Lauritzen <kplauritzen@gmail.com>
2018-09-28 17:19:59 +0200

    Added logging and Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 """
 Flask application instance
 """
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -18,5 +22,19 @@ login.login_view = 'login'
 boostrap = Bootstrap(app=app)
 moment = Moment(app)
 
+if app.config['LOG_TO_STDOUT']:
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    app.logger.addHandler(stream_handler)
+else:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log',
+        maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
 
 from app import routes, models
