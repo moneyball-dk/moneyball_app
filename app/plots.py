59d5d1b783e99be00696ed4b909ac9dca552b70b
Kasper Primdal Lauritzen <kplauritzen@gmail.com>
2018-11-06 08:51:10 +0100

    updates plots

--- app/plots.py
+++ app/plots.py
@@ -1,5 +1,6 @@
-from bokeh.plotting import figure
+from bokeh.plotting import figure, ColumnDataSource
 from bokeh.embed import components
+from bokeh.models import HoverTool
 
 from app.models import User, Rating
 
@@ -13,13 +14,31 @@ def get_ratings(shortname, rating_type='elo'):
     ratings = Rating.query \
         .filter_by(rating_type=rating_type) \
         .filter_by(user_id=user.id) \
-        .order_by(Rating.timestamp)
-    values = [r.rating_value for r in ratings.all()]
-    dates = datetime([r.timestamp for r in ratings.all()])
-    return dates, values
+        .order_by(Rating.timestamp).all()
+    values = [r.rating_value for r in ratings]
+    dates = datetime([r.timestamp for r in ratings])
+    match_ids = [r.match_id for r in ratings]
+    source = ColumnDataSource(data=dict(
+        date=dates,
+        rating=values,
+        match_id=match_ids
+    ))
+    return source
 
 def plot_ratings(shortname, rating_type):
-    dates, values = get_ratings(shortname, rating_type=rating_type)
-    p = figure(x_axis_type="datetime")
-    p.line(dates, values)
+    source = get_ratings(shortname, rating_type=rating_type)
+    p = figure(x_axis_type="datetime", tools="",)
+    p.line('date', 'rating', source=source)
+
+    p.add_tools(HoverTool(
+        tooltips=[
+            ('match_id', '@match_id'),
+            ('rating', '@rating{0.2f}'),
+            ('date', '@date{%F}'),
+        ],
+        formatters={
+            'date':'datetime'
+        },
+        mode='vline',
+    ))
     return p
