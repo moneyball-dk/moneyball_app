1adf99c3a136ff0487d992865e8d6207a39a9f46
Kasper Primdal Lauritzen <kplauritzen@gmail.com>
2018-11-22 14:11:10 +0100

    HACKERMAN

--- app/routes.py
+++ app/routes.py
@@ -170,6 +170,22 @@ def route_edit_password():
         return redirect(url_for('user', user_id=user.id))
     return render_template('edit_user_password.html', title='Moneyball', form=form)
 
+#@app.route('/edit_user_password', methods=['GET', 'POST'])
+@app.route('/user/<user_id>/edit', methods=['GET', 'POST'])
+@login_required
+def route_edit_password(user_id):
+    form = EditPasswordForm()
+    #user = current_user
+    user = User.query.filter_by(id=user_id).first_or_404()
+    if form.validate_on_submit():
+        tasks.update_password(
+            user=user,
+            password=form.password.data,
+        )
+        flash(f'Password of user {user} updated')
+        return redirect(url_for('user', user_id=user.id))
+    return render_template('edit_user_password.html', title='Moneyball', form=form)
+
 
 @app.route('/match/<match_id>/approve', methods=['POST'])
 @login_required
